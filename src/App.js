d688372fab9b4b8af2c76d21401fd030b455ed99
Zevi Liao <jointjm@gmail.com>
2019-08-14 10:15:51 +0800

    counter v1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import './App.css'
 import * as rxjs from 'rxjs'
-import { map } from 'rxjs/operators'
+import { map, scan } from 'rxjs/operators'
 // import { fromEvent } from 'rxjs';
 
-rxjs.of(1, 2, 3).subscribe(n => {
-  console.log(n)
-})
+// rxjs.of(1, 2, 3).subscribe(n => {
+//   console.log(n)
+// })
 
 // rxjs.fromEvent(window, 'click').subscribe(e => {
 //   console.log('click~')
 // })
 
+// rxjs
+//   .fromEvent(window, 'click')
+//   .pipe(map(e => e.target))
+//   .subscribe(value => {
+//     console.log('click: ', value)
+//   })
+
 rxjs
   .fromEvent(window, 'click')
-  .pipe(map(e => e.target))
+  .pipe(
+    map(e => 1),
+    scan((total, now) => total + now)
+  )
   .subscribe(value => {
-    console.log('click: ', value)
+    document.querySelector('#counter').innerText = value
   })
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header">
+        click count:
+        <div id="counter">hello</div>
+      </header>
     </div>
   )
 }
