b1faa213a422a1fb75cab4a49a6d944b89cd0d29
Zevi Liao <jointjm@gmail.com>
2019-08-14 09:21:19 +0800

    add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import * as rxjs from 'rxjs'
+import { map } from 'rxjs/operators'
+// import { fromEvent } from 'rxjs';
+
+rxjs.of(1, 2, 3).subscribe(n => {
+  console.log(n)
+})
+
+// rxjs.fromEvent(window, 'click').subscribe(e => {
+//   console.log('click~')
+// })
+
+rxjs
+  .fromEvent(window, 'click')
+  .pipe(map(e => e.target))
+  .subscribe(value => {
+    console.log('click: ', value)
+  })
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">hello</header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
